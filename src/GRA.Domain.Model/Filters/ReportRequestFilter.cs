f72f743af5999042490c8fdbee73aa3b9a4837ce
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2025-09-04 15:11:54 -0700

    Add basic model for a page that displays reports that have been run

---
+++ src/GRA.Domain.Model/Filters/ReportRequestFilter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GRA.Domain.Model.Filters
+{
+    internal class ReportRequestFilter : BaseFilter
+    {
+        public int? ReportId { get; set; }
+        public int? RequestedByUserId { get; set; }
+    }
+}
