5bf2ae848236494974ee00c3c45312889ecfe648
Adam Abrams-Flohr <AdamAbrams-Flohr@mcldaz.org>
2026-06-03 15:05:13 -0700

    Create a viewmodel for avatar background uploads

---
+++ src/GRA.Controllers/ViewModel/MissionControl/Avatar/BackgroundViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace GRA.Controllers.ViewModel.MissionControl.Avatar
+{
+    public class BackgroundViewModel
+    {
+        public bool BackgroundExists { get; set; }
+        public string BackgroundImageUrl { get; set; }
+        public IFormFile UploadedFile { get; set; }
+    }
+}
